ec65c7925aa5b728531417e04c99318e92dbb635
Will Pearce <william.pearce@numonix.io>
2025-04-10 14:39:54 -0400

    add check for go install

--- runkey.cpp
+++ runkey.cpp
@@ -7,6 +7,12 @@ namespace fs = std::filesystem;
 
 int main()
 {
+  if (system("go version >nul 2>&1"))
+  {
+    MessageBox(NULL, TEXT("Go is not installed or not in PATH."), TEXT("Error"), MB_OK | MB_ICONERROR);
+    return 1;
+  }
+
   TCHAR pathBuffer[MAX_PATH] = {0};
   GetModuleFileName(NULL, pathBuffer, MAX_PATH);
   fs::path p(pathBuffer);
